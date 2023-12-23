85510d312cbac5a3bfdeafc4c88e092abfe4f827
Danila <146864292+wouryUwU@users.noreply.github.com>
2023-12-23 13:20:32 +0300

    Add files via upload

---
+++ scence/main menu.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+
+
+func _on_lvl_1_pressed():
+	get_tree().change_scene_to_file("res://scence/lvl1.tscn")
+
+
+func _on_lvl_2_pressed():
+	get_tree().change_scene_to_file("res://scence/lvl2.tscn")
